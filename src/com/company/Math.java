a67dad0d625995fe96830ebecf57670c56a4c068
Yuliya513 <yuliya.senik@gmail.com>
2021-05-14 16:54:17 +0300

    Initial commit

---
+++ src/com/company/Math.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Math {
+
+    public static void main(String[] args) {
+	double num=10.53;
+        try {
+            double answer;
+            answer=Math.sqrt(num1);
+            System.out.println("The square");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
